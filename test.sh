a4adb8cf8b2bca9818aa8d17b3d16ac488f513a1
Daniel Hoang <danielvanhoang@gmail.com>
2022-01-31 21:55:52 -0600

    test number 6

--- test.sh
+++ test.sh
@@ -2,6 +2,14 @@
 value=$(curl -XGET "https://dhpup-eck.es.us-west-1.aws.found.io:9243/apm-7.16.3-transaction/_search" --header 'Authorization: ApiKey MGFKam9uNEJCWFhBTnc0WUZDRms6c0Z3cnRQTGdTbWl1LWltQ2RvM01xUQ==' -H 'Content-Type: application/json' -d'
 {
   "size": 0,
+  "query": {
+    "range": {
+      "@timestamp": {
+        "gte": "now-1m/m",
+        "lt": "now/m"
+      }
+    }
+  },
   "aggs": {
     "review_average": {
       "avg": {
